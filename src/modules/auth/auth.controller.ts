29475a07f8b25628a82af0ebf71a44d6eef705fb
viettq3 <viettq3@rikkeisoft.com>
2024-11-14 09:44:53 +0700

    specify response code for auth

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ApiTags } from '@nestjs/swagger';
 import { SignUpDto } from './dtos/sign-up.dto';
@@ -12,12 +12,16 @@ export class AuthController {
 
   @Public()
   @Post('sign-in')
+  @HttpCode(HttpStatus.OK)
+  @HttpCode(HttpStatus.BAD_REQUEST)
   async signIn(@Body() signInDto: SignInDto) {
     return await this.authService.signIn(signInDto.email, signInDto.password);
   }
 
   @Public()
   @Post('sign-up')
+  @HttpCode(HttpStatus.CREATED)
+  @HttpCode(HttpStatus.BAD_REQUEST)
   async signUp(@Body() signUpDto: SignUpDto) {
     return await this.authService.signUp(signUpDto);
   }
